d7f64078868ee3597e694a0decfacb10994546be
Zhihao Cui <davidcui2@me.com>
2022-08-01 22:52:31 +0100

    Refactor to add figma dark theme support

--- ui-src/App.tsx
+++ ui-src/App.tsx
@@ -1,119 +1,23 @@
-import {
-  Button,
-  Checkbox,
-  FlexLayout,
-  FormField,
-  Input,
-  ToolkitProvider,
-  StackLayout,
-} from "@jpmorganchase/uitk-core";
-import { Dropdown } from "@jpmorganchase/uitk-lab";
-import React, { useEffect, useRef, useState } from "react";
-import {
-  ExportColorAllFormats,
-  ExportColorFormat,
-  PostToFigmaMessage,
-  PostToUIMessage,
-} from "../shared-src";
+import { ToolkitProvider } from "@jpmorganchase/uitk-core";
+import React, { useState, useEffect } from "react";
+import { MainView } from "./views/MainView";
 
 import "./App.css";
+import "./Icon.css"; // icon bug from uitk-icons@0.2.0 & 0.3.0
 
 function App() {
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
-  const copyButtonRef = useRef<HTMLButtonElement>(null);
-
-  const [text, setText] = useState("");
-  const [prefix, setPrefix] = useState("");
-  const [format, setFormat] = useState<ExportColorFormat>(
-    ExportColorAllFormats[0]
-  );
-  const [ignoreFirstGroup, setIgnoreFirstGroup] = useState(false);
-  const [ignoreDefaultEnding, setIgnoreDefaultEnding] = useState(false);
-
-  const onExport = () => {
-    parent.postMessage(
-      {
-        pluginMessage: {
-          type: "export-css",
-          prefix,
-          format,
-          ignoreFirstGroup,
-          ignoreDefaultEnding,
-        } as PostToFigmaMessage,
-      },
-      "*"
-    );
-  };
-
-  const onCopy = () => {
-    textareaRef.current?.select();
-    document.execCommand("copy");
-    copyButtonRef.current?.focus();
-  };
-
-  const handleMessage = (event: MessageEvent) => {
-    const pluginMessage = event.data.pluginMessage as PostToUIMessage;
-    switch (pluginMessage.type) {
-      case "generated":
-        setText(pluginMessage.data.join("\n"));
-        break;
-      default:
-        break;
-    }
-  };
+  const [theme, setTheme] = useState<"light" | "dark">("light");
 
   useEffect(() => {
-    window.addEventListener("message", handleMessage);
-
-    return () => {
-      window.removeEventListener("message", handleMessage);
-    };
+    // Support Figma dark theme - https://www.figma.com/plugin-docs/css-variables/
+    if (document.querySelector("html")?.classList.contains("figma-dark")) {
+      setTheme("dark");
+    }
   }, []);
 
   return (
-    <ToolkitProvider>
-      <StackLayout gap={1}>
-        <FormField label="Prefix" labelPlacement="left">
-          <Input
-            value={prefix}
-            onChange={(e) => setPrefix(e.currentTarget.value)}
-          ></Input>
-        </FormField>
-        <FormField label="Format" labelPlacement="left">
-          <Dropdown
-            source={ExportColorAllFormats as any}
-            selectedItem={format}
-            onChange={(_, item) => {
-              if (item) {
-                setFormat(item);
-              }
-            }}
-          />
-        </FormField>
-        <FlexLayout>
-          <Checkbox
-            label="Extract by first group"
-            checked={ignoreFirstGroup}
-            onChange={(_, checked) => setIgnoreFirstGroup(checked)}
-          />
-          <Checkbox
-            label="Trim default ending"
-            checked={ignoreDefaultEnding}
-            onChange={(_, checked) => setIgnoreDefaultEnding(checked)}
-          />
-        </FlexLayout>
-        <Button onClick={onExport}>Export</Button>
-        <textarea
-          rows={20}
-          value={text}
-          onChange={(e) => setText(e.currentTarget.value)}
-          ref={textareaRef}
-          spellCheck={false}
-        ></textarea>
-        <Button onClick={onCopy} ref={copyButtonRef}>
-          Copy
-        </Button>
-      </StackLayout>
+    <ToolkitProvider theme={theme}>
+      <MainView />
     </ToolkitProvider>
   );
 }
---
+++ ui-src/views/MainView.tsx
@@ -0,0 +1,114 @@
+import {
+  Button,
+  Checkbox,
+  FlexLayout,
+  FormField,
+  Input,
+  StackLayout,
+} from "@jpmorganchase/uitk-core";
+import { Dropdown } from "@jpmorganchase/uitk-lab";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  ExportColorAllFormats,
+  ExportColorFormat,
+  PostToFigmaMessage,
+  PostToUIMessage,
+} from "../../shared-src";
+
+export const MainView = () => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const copyButtonRef = useRef<HTMLButtonElement>(null);
+
+  const [text, setText] = useState("");
+  const [prefix, setPrefix] = useState("");
+  const [format, setFormat] = useState<ExportColorFormat>(
+    ExportColorAllFormats[0]
+  );
+  const [ignoreFirstGroup, setIgnoreFirstGroup] = useState(false);
+  const [ignoreDefaultEnding, setIgnoreDefaultEnding] = useState(false);
+
+  const onExport = () => {
+    parent.postMessage(
+      {
+        pluginMessage: {
+          type: "export-css",
+          prefix,
+          format,
+          ignoreFirstGroup,
+          ignoreDefaultEnding,
+        } as PostToFigmaMessage,
+      },
+      "*"
+    );
+  };
+
+  const onCopy = () => {
+    textareaRef.current?.select();
+    document.execCommand("copy");
+    copyButtonRef.current?.focus();
+  };
+
+  const handleMessage = (event: MessageEvent) => {
+    const pluginMessage = event.data.pluginMessage as PostToUIMessage;
+    switch (pluginMessage.type) {
+      case "generated":
+        setText(pluginMessage.data.join("\n"));
+        break;
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("message", handleMessage);
+
+    return () => {
+      window.removeEventListener("message", handleMessage);
+    };
+  }, []);
+
+  return (
+    <StackLayout gap={1}>
+      <FormField label="Prefix" labelPlacement="left">
+        <Input
+          value={prefix}
+          onChange={(e) => setPrefix(e.currentTarget.value)}
+        ></Input>
+      </FormField>
+      <FormField label="Format" labelPlacement="left">
+        <Dropdown
+          source={ExportColorAllFormats}
+          selectedItem={format}
+          onChange={(_, item) => {
+            if (item) {
+              setFormat(item);
+            }
+          }}
+        />
+      </FormField>
+      <FlexLayout>
+        <Checkbox
+          label="Extract by first group"
+          checked={ignoreFirstGroup}
+          onChange={(_, checked) => setIgnoreFirstGroup(checked)}
+        />
+        <Checkbox
+          label="Trim default ending"
+          checked={ignoreDefaultEnding}
+          onChange={(_, checked) => setIgnoreDefaultEnding(checked)}
+        />
+      </FlexLayout>
+      <Button onClick={onExport}>Export</Button>
+      <textarea
+        rows={20}
+        value={text}
+        onChange={(e) => setText(e.currentTarget.value)}
+        ref={textareaRef}
+        spellCheck={false}
+      ></textarea>
+      <Button onClick={onCopy} ref={copyButtonRef}>
+        Copy
+      </Button>
+    </StackLayout>
+  );
+};
