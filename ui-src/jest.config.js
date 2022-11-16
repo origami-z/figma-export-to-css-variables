bd4e0d2f0e6ede4820457fffdb0c6fddc779c4cd
origami-z <davidcui2@me.com>
2022-11-16 09:23:23 +0000

    Simplify ui test tsconfig

--- ui-src/jest.config.js
+++ ui-src/jest.config.js
@@ -2,16 +2,6 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
-  transform: {
-    // Below matches default jest transform (from --debug)
-    // or a second transform would be applied and tsconfig would be overridden
-    "^.+\\.tsx?$": [
-      "ts-jest",
-      {
-        tsconfig: "ui-src/__tests__/tsconfig.json",
-      },
-    ],
-  },
   setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
   moduleNameMapper: {
     ".+\\.(css|ttf|woff|woff2)$": "identity-obj-proxy",
